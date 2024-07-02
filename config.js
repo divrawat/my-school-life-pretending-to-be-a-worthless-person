4679602fd2d6d1072cb7f2fac9a34fa62f8ac82d
divrawat <divrawat2001@gmail>
2024-07-03 00:22:54 +0530

    first commit

---
+++ config.js
@@ -0,0 +1,362 @@
+
+// export const DOMAIN = 'http://localhost:3000';
+export const DOMAIN = 'https://www.myschoollifepretendingtobeaworthlessperson.in.net';
+
+
+export const MANGA_DESCRIPTION0 = ``;
+
+export const MANGA_DESCRIPTION = `<a href="${DOMAIN}"><u>My School Life Pretending To be a Worthless Person</u></a> dives into an unconventional school experience. The story follows a protagonist who chooses a peculiar path: pretending to be worthless. There could be several reasons behind this facade. Perhaps the protagonist possesses hidden abilities they want to conceal.`;
+
+export const MANGA_NAME = 'My School Life Pretending To be a Worthless Person';
+export const MANGA_AUTHOR = 'Sandfairy';
+export const MANGA_RELEASE = '2019';
+export const MANGA_STATUS = 'Ongoing';
+export const MANGA_ARTIST = 'Sandfairy';
+export const MANGA_STUDIO = 'None';
+export const MANGA_GENRE = 'Action, Adventure, Fantasy, Horror, Romance';
+
+export const CHAPTER_PREFIX = 'my-school-life-pretending-to-be-a-worthless-person-chapter';
+export const IMAGE_PREFIX = 'images/my-school-life-pretending-to-be-a-worthless-person';
+export const NEXT_PREVIOUS_PREFIX = 'manga/my-school-life-pretending-to-be-a-worthless-person-chapter';
+export const URL_PREFIX = 'manga/my-school-life-pretending-to-be-a-worthless-person-chapter';
+
+export const AUTHOR_PAGE = `${DOMAIN}/my-school-life-pretending-to-be-a-worthless-person-team`;
+export const LOGO_URL = `${DOMAIN}/logo.webp`;
+export const COVER_IMG = `${DOMAIN}/cover.webp`;
+export const BEHIND_COVER_IMG = "https://otakukart.com/wp-content/uploads/2023/04/My-School-Life-Pretending-To-Be-a-Worthless-Person-2-1.jpg";
+
+
+// Navbar
+export const logo = <img src={`${LOGO_URL}`} alt="Logo" className="h-[75px] rounded-full w-[75px] mr-5 md:ml-0 ml-4 md:pb-2 md:my-0 my-1" />
+
+export const APP_NAME = 'My School Life Pretending To be a Worthless Person';
+export const NavbarName = "MSLPTBAWP";
+
+export const APP_DESCRIPTION = `Read ${MANGA_NAME} manga online at ${DOMAIN}. The story follows a protagonist who chooses a peculiar path: pretending to be worthless. There could be several reasons behind this facade. Perhaps the protagonist possesses hidden abilities they want to conceal.`;
+
+export const DISQUS_SHORTNAME = "my-cms-7";
+
+
+export const MANGA_SUMMARY = [
+    {
+        id: 1,
+        content: "My School Life Pretending To Be a Worthless Person delves into the psychological depths of a student who embarks on a bizarre social experiment: acting completely worthless. This isn't your typical high school story; the protagonist, whose name remains undisclosed, carves a unique path shrouded in mystery."
+    },
+    {
+        id: 2,
+        content: "The reasons behind this charade are shrouded in intrigue. Does the protagonist possess extraordinary abilities they're desperately hiding? Or is it a silent rebellion against the relentless pressure to excel in a society obsessed with academic achievement? Perhaps it's a carefully crafted shield to deflect unwanted attention or a way to manipulate others from the shadows."
+    },
+    {
+        id: 3,
+        content: "Whatever the motivation, pretending to be worthless comes at a cost. The protagonist becomes a social pariah, enduring isolation and relentless underestimation. Yet, within this self-imposed exile, a strange sense of liberation emerges. Free from the shackles of expectation, they navigate the school hierarchy from a unique vantage point."
+    },
+    {
+        id: 4,
+        content: "The story explores the psychological toll of such a performance. Does the mask eventually become the true face? Does the act of pretending blur the lines between reality and self-perception?  The narrative delves into the protagonist's internal struggle, the emotional toll of living a lie, and the yearning for genuine connection despite the self-imposed isolation."
+    },
+    {
+        id: 5,
+        content: "But My School Life Pretending To Be a Worthless Person is more than just a character study. The unique blend of genres – action, adventure, fantasy, horror, and even a dash of romance – hints at a deeper story waiting to unfold.  Are the protagonist's hidden abilities more than just a theory?  Could there be a fantastical world lurking beneath the surface of everyday school life, a world the protagonist is desperately trying to avoid or perhaps manipulate?"
+    },
+    {
+        id: 6,
+        content: "The horror element adds a layer of suspense. Are there hidden dangers lurking in the shadows, threats the protagonist's charade can't fully protect them from?  And what about the romance? Does a connection with another character force the protagonist to confront their facade or offer a glimpse of vulnerability beneath the mask?"
+    },
+    {
+        id: 7,
+        content: "As the story progresses, secrets are sure to be unveiled. Will the protagonist's true nature be revealed? Will their hidden abilities come to light, and if so, how will they use them? My School Life Pretending To Be a Worthless Person is a captivating exploration of identity, deception, and the unexpected consequences of defying expectations. It's a story that promises to keep you guessing until the very end."
+    }
+];
+
+
+
+
+export const navLinks = [
+    { text: 'Home', href: `${DOMAIN}` },
+    { text: 'DMCA', href: `${DOMAIN}/dmca` },
+    { text: 'Terms & Conditions', href: `${DOMAIN}/terms-and-conditions` },
+];
+
+export const FooterLinks = [
+    { text: 'About', href: `${DOMAIN}/about` },
+    { text: 'Contact', href: `${DOMAIN}/contact` },
+    { text: 'Disclaimer', href: `${DOMAIN}/disclaimer` },
+    { text: 'Privacy Policy', href: `${DOMAIN}/privacy-policy` },
+    { text: 'Terms & Conditions', href: `${DOMAIN}/terms-and-conditions` }
+];
+
+
+
+export const RelatedMangaLinks = [
+    /*
+    {
+        imageUrl: `${DOMAIN}/related/frieran.webp`,
+        title: 'Frieran Manga',
+        link: 'https://www.frierenmanga.in.net',
+    },
+    */
+
+];
+
+
+
+
+
+export const chaptersData = [
+    {
+        "chapterNumber": "1",
+        "numImages": 12
+    },
+    {
+        "chapterNumber": "2",
+        "numImages": 9
+    },
+    {
+        "chapterNumber": "3",
+        "numImages": 10
+    },
+    {
+        "chapterNumber": "4",
+        "numImages": 11
+    },
+    {
+        "chapterNumber": "5",
+        "numImages": 11
+    },
+    {
+        "chapterNumber": "6",
+        "numImages": 8
+    },
+    {
+        "chapterNumber": "7",
+        "numImages": 9
+    },
+    {
+        "chapterNumber": "8",
+        "numImages": 10
+    },
+    {
+        "chapterNumber": "9",
+        "numImages": 11
+    },
+    {
+        "chapterNumber": "10",
+        "numImages": 11
+    },
+    {
+        "chapterNumber": "11",
+        "numImages": 11
+    },
+    {
+        "chapterNumber": "12",
+        "numImages": 10
+    },
+    {
+        "chapterNumber": "13",
+        "numImages": 10
+    },
+    {
+        "chapterNumber": "14",
+        "numImages": 10
+    },
+    {
+        "chapterNumber": "15",
+        "numImages": 11
+    },
+    {
+        "chapterNumber": "16",
+        "numImages": 11
+    },
+    {
+        "chapterNumber": "17",
+        "numImages": 11
+    },
+    {
+        "chapterNumber": "18",
+        "numImages": 12
+    },
+    {
+        "chapterNumber": "19",
+        "numImages": 11
+    },
+    {
+        "chapterNumber": "20",
+        "numImages": 12
+    },
+    {
+        "chapterNumber": "21",
+        "numImages": 11
+    },
+    {
+        "chapterNumber": "22",
+        "numImages": 13
+    },
+    {
+        "chapterNumber": "23",
+        "numImages": 12
+    },
+    {
+        "chapterNumber": "24",
+        "numImages": 10
+    },
+    {
+        "chapterNumber": "25",
+        "numImages": 11
+    },
+    {
+        "chapterNumber": "26",
+        "numImages": 12
+    },
+    {
+        "chapterNumber": "27",
+        "numImages": 12
+    },
+    {
+        "chapterNumber": "28",
+        "numImages": 10
+    },
+    {
+        "chapterNumber": "29",
+        "numImages": 9
+    },
+    {
+        "chapterNumber": "30",
+        "numImages": 11
+    },
+    {
+        "chapterNumber": "31",
+        "numImages": 11
+    },
+    {
+        "chapterNumber": "32",
+        "numImages": 10
+    },
+    {
+        "chapterNumber": "33",
+        "numImages": 13
+    },
+    {
+        "chapterNumber": "34",
+        "numImages": 10
+    },
+    {
+        "chapterNumber": "35",
+        "numImages": 10
+    },
+    {
+        "chapterNumber": "36",
+        "numImages": 10
+    },
+    {
+        "chapterNumber": "37",
+        "numImages": 9
+    },
+    {
+        "chapterNumber": "38",
+        "numImages": 17
+    },
+    {
+        "chapterNumber": "39",
+        "numImages": 16
+    },
+    {
+        "chapterNumber": "40",
+        "numImages": 26
+    },
+    {
+        "chapterNumber": "41",
+        "numImages": 10
+    },
+    {
+        "chapterNumber": "42",
+        "numImages": 10
+    },
+    {
+        "chapterNumber": "43",
+        "numImages": 9
+    },
+    {
+        "chapterNumber": "44",
+        "numImages": 11
+    },
+    {
+        "chapterNumber": "45",
+        "numImages": 24
+    },
+    {
+        "chapterNumber": "46",
+        "numImages": 22
+    },
+    {
+        "chapterNumber": "47",
+        "numImages": 10
+    },
+    {
+        "chapterNumber": "48",
+        "numImages": 28
+    },
+    {
+        "chapterNumber": "49",
+        "numImages": 15
+    },
+    {
+        "chapterNumber": "50",
+        "numImages": 8
+    },
+    {
+        "chapterNumber": "51",
+        "numImages": 29
+    },
+    {
+        "chapterNumber": "52",
+        "numImages": 10
+    },
+    {
+        "chapterNumber": "53",
+        "numImages": 12
+    },
+    {
+        "chapterNumber": "54",
+        "numImages": 22
+    },
+    {
+        "chapterNumber": "55",
+        "numImages": 22
+    },
+    {
+        "chapterNumber": "56",
+        "numImages": 26
+    },
+    {
+        "chapterNumber": "57",
+        "numImages": 20
+    },
+    {
+        "chapterNumber": "58",
+        "numImages": 15
+    },
+    {
+        "chapterNumber": "59",
+        "numImages": 15
+    },
+    {
+        "chapterNumber": "60",
+        "numImages": 13
+    },
+    {
+        "chapterNumber": "61",
+        "numImages": 32
+    },
+    {
+        "chapterNumber": "62",
+        "numImages": 12
+    },
+    {
+        "chapterNumber": "63",
+        "numImages": 10
+    },
+    {
+        "chapterNumber": "64",
+        "numImages": 12
+    }
+]
